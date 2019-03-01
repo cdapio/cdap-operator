5c01bc82004ad3f8082129141a33bf04158507db
Terence Yim <terencey@google.com>
2019-03-01 09:10:38 -0800

    Defining cdap master types

--- pkg/apis/cdap/v1alpha1/cdapmaster_types.go
+++ pkg/apis/cdap/v1alpha1/cdapmaster_types.go
@@ -17,7 +17,34 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ServiceType is the name identifying various CDAP master services
+type ServiceType string
+
+const (
+	// AppFabric defines the service type for app-fabric
+	AppFabric ServiceType = "AppFabric"
+
+	// Log defines the service type for log processing and serving service
+	Log ServiceType = "Log"
+
+	// Messaging defines the service type for TMS
+	Messaging ServiceType = "Messaging"
+
+	// Metadata defines the service type for metadata service
+	Metadata ServiceType = "Metadata"
+
+	// Metrics defines the service type for metrics process and serving
+	Metrics ServiceType = "Metrics"
+
+	// Router defines the service type for the router
+	Router ServiceType = "Router"
+
+	// UserInterface defines the service type for user interface
+	UserInterface ServiceType = "UserInterface"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -27,12 +54,31 @@ import (
 type CDAPMasterSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Image           string                  `json:"image,omitempty"`
+	ImagePullPolicy corev1.PullPolicy       `json:"imagePullPolicy,omitempty"`
+	Services        []CDAPMasterService     `json:"services"`
+	SecuritySecret  *corev1.SecretReference `json:"securitySecret,omitempty"`
+	LocationURI     string                  `json:"locationURI"`
+	Config          map[string]string       `json:"config,omitempty"`
+}
+
+// CDAPMasterService defines specification for one CDAP master service
+type CDAPMasterService struct {
+	Type         ServiceType                       `json:"type"`
+	Instances    *int32                            `json:"instances"`
+	Resources    *corev1.ResourceRequirements      `json:"resources,omitempty"`
+	VolumeSpec   *corev1.PersistentVolumeClaimSpec `json:"volumeSpec,omitempty"`
+	NodeSelector map[string]string                 `json:"nodeSelector,omitempty"`
 }
 
 // CDAPMasterStatus defines the observed state of CDAPMaster
 type CDAPMasterStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,1,opt,name=observedGeneration"`
+
+	RouterService        string `json:"routerService,omitempty"`
+	UserInterfaceService string `json:"userInterfaceService,omitempty"`
 }
 
 // +genclient
