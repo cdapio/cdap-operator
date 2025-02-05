671572a150ce4e4474dc48c4d5d88da185e0f67b
Anshuman <anshumanks@google.com>
2025-02-05 20:25:54 +0530

    Fixed tests

--- controllers/version_update_test.go
+++ controllers/version_update_test.go
@@ -214,7 +214,8 @@ var _ = Describe("Controller Suite", func() {
 					ImageToUse:             curUIImage,
 				},
 			}
-			postJobSpec := buildPreUpgradeJobSpec(getPreUpgradeJobName(master.Status.UpgradeStartTimeMillis), master, emptyLabels, true)
+			// Force the pre upgrade job configurations
+			postJobSpec := buildPreUpgradeJobSpec(getPreUpgradeJobName(master.Status.UpgradeStartTimeMillis), master, emptyLabels, false)
 			object, err := buildUpgradeJobObject(postJobSpec)
 			Expect(err).To(BeNil())
 
