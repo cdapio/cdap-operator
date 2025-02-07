996a2b537b5133b5b270bcc58b0cef272264c551
Anshuman <anshumanks@google.com>
2025-02-07 19:04:20 +0530

    asd

--- controllers/spec.go
+++ controllers/spec.go
@@ -597,11 +597,11 @@ func newUpgradeJobSpec(master *v1alpha1.CDAPMaster, name string, labels map[stri
 	s.CConf = cconf
 	s.HConf = hconf
 
-// 	if val, exists := master.Spec.Config[confSkipPreUpgradeFlag]; exists {
-//     s.SkipPreUpgradeFlag = (val == "true")
-//   } else {
-  s.SkipPreUpgradeFlag = false
-//   }
+	if val, exists := master.Spec.Config[confSkipPreUpgradeFlag]; exists {
+    s.SkipPreUpgradeFlag = (val == "true")
+  } else {
+    s.SkipPreUpgradeFlag = false
+  }
 	s.SkipPreUpgrade = false
 
 	return s
@@ -619,9 +619,12 @@ func (s *VersionUpgradeJobSpec) SetPostUpgrade(isPostUpgrade bool) *VersionUpgra
 
 func (s *VersionUpgradeJobSpec) SetSkipPreUpgrade(isPatchUpgrade bool) *VersionUpgradeJobSpec {
   // If it is a patch revision and the flag is true, skip the pre upgrade job
+	fmt.Println("SkipPreUpgradeFlag:")
+	fmt.Println(s.SkipPreUpgradeFlag)
 	s.SkipPreUpgrade = isPatchUpgrade && s.SkipPreUpgradeFlag
-	fmt.Println("LOGS:")
+	fmt.Println("isPatchUpgrade:")
 	fmt.Println(isPatchUpgrade)
+	fmt.Println("SkipPreUpgradeFlag:")
 	fmt.Println(s.SkipPreUpgradeFlag)
 	return s
 }
