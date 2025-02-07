b0740db6a95a5f8e120ebd95f455b717c1068157
Anshuman <anshumanks@google.com>
2025-02-07 19:08:19 +0530

    asdfg

--- controllers/spec.go
+++ controllers/spec.go
@@ -613,6 +613,14 @@ func (s *VersionUpgradeJobSpec) SetPostUpgrade(isPostUpgrade bool) *VersionUpgra
 
 func (s *VersionUpgradeJobSpec) SetSkipPreUpgrade(isPatchUpgrade bool) *VersionUpgradeJobSpec {
   // If it is a patch revision and the flag is true, skip the pre upgrade job
+	fmt.Println("isPatchUpgrade:")
+	fmt.Println(isPatchUpgrade)
+	fmt.Println("s.SkipPreUpgradeFlag:")
+	fmt.Println(s.SkipPreUpgradeFlag)
 	s.SkipPreUpgrade = isPatchUpgrade && s.SkipPreUpgradeFlag
+	fmt.Println("isPatchUpgrade:")
+	fmt.Println(isPatchUpgrade)
+	fmt.Println("s.SkipPreUpgradeFlag:")
+	fmt.Println(s.SkipPreUpgradeFlag)
 	return s
 }
